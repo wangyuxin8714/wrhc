8a4270cc82d423a98a56e4d38f9be0db8a967765
wyx <1510172244@qq.com>
2019-07-10 19:07:16 +0800

    首页

--- Prosperity/src/components/dialog.vue
+++ Prosperity/src/components/dialog.vue
@@ -1,24 +1,49 @@
 <template>
     <div class='dialog'>
         <div class="box">
-            <div class="top">
-                <span>颜色
-                    <img :src="url" alt=""/>
-                </span>
-                <span @click="$emit('cancleClick',flag)">X</span>
+            <div class="header">
+                <p>
+                    <span>花色</span>
+                    <span>尺寸</span>
+                </p>
+                <p>X</p>
             </div>
-            <div class="center" v-for="(item,index) in arr" :key="index">
-                   <p>{{item.aname}}</p>
-                   <ul class="list">
-                       <li v-for="(val,index1) in item.attributeValueRelationVoList" :key="index1" :class="{'active':index1==ind[index]}"  @click="chooseClick(index,index1)">
-                           {{val.vname}}
-                       </li>
-                   </ul> 
+            <dl>
+                <dt>
+                    <img src="https://jnup.oss-cn-beijing.aliyuncs.com/product/28a3c7a08b7199887e50e7e64f0250df.jpg" alt="">
+                </dt>
+                <dd>
+                    <p>￥135</p>
+                    <p>库存：100</p>
+                </dd>
+            </dl>
+            <div class="colors">
+                <p>花色</p>
+                <ul>
+                    <li>绅士</li>
+                    <li>绅士</li>
+                    <li>绅士</li>
+                </ul>
             </div>
-                <my-Count :num="num" @subClick="sub" @addClick="add"/>
-                <div class="footer">
-                    <button  v-on:click="shopBtn" @click="$emit('cancleClick',flag)">确定</button>
-                 </div>
+            <div class="colors">
+                <p>尺寸</p>
+                <ul>
+                    <li>0.9米床</li>
+                    <li>0.9米床</li>
+                    <li>0.9米床</li>
+                </ul>
+            </div>
+            <div class="numbox">
+                <p>数量</p>
+                <div class="num">
+                    <span>-</span>
+                    <span class="number">1</span>
+                    <span>+</span>
+                </div>
+            </div>
+            <button>
+                确定
+            </button>
         </div> 
     </div>
 </template>
@@ -32,119 +57,133 @@ export default {
     },
     data(){
         return {
-         ind:[],
-         buyArr:[],
-         num:1
+         
         }
     },
     computed:{
 
     },
     methods:{
-        chooseClick(ind,ind1){
-         this.ind=this.ind.map((val,key)=>{      
-                if(key==ind){
-                    val=ind1
-                }
-                return val
-        })
-        },
-        sub(){
-            this.num=this.num>1?this.num-1:1
-        },
-        add(){
-            this.num=this.num+1;
-        },
-        shopBtn(){
-            this.flag=false;
-            this.ind.map((val,index)=>{
-              this.buyArr.push(this.arr[index].attributeValueRelationVoList[val])
-           })
-           this.buyArr.push(this.num)
-        }
-    },
-    created(){
-    
-    },
-    mounted(){
-
-    },
-    onLoad(){
-        this.arr.forEach(val=>{
-            if(val){
-                this.ind.push(0)
-            }
-        })
     }
 }
 </script>
 <style  scoped lang="scss">
-    .dialog{
-        position: fixed;
-        top:0;
-        left:0;
-        height: 100%;
+.dialog{
+    position: fixed;
+    top:0;
+    left:0;
+    height: 100%;
+    width: 100%;
+    background: rgba(0,0,0,.3);
+    .box{
         width: 100%;
-        background: rgba(0,0,0,.3);
-        .box{
-            width: 100%;
-            height: 80%;
-            background: #fff;
-            position: absolute;
-            bottom: 0;
-            .top{
-                width: 100%;
-                height: 65px;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                padding: 5px 10px;
-                box-sizing: border-box;
-                img{
-                    width: 50px;
-                    height: 50px;
-                }
-            }
-            .center{
-                width: 100%;
-                height: 25%;
-                overflow-y: scroll;    
-                p{
-                    padding: 0 10px;
-                    color: #999;
-                    font-size: 17px;
-                }
-                ul{
-                    display: flex;
-                    flex-wrap: wrap;
-                    li{
-                        margin:5px;
-                        padding:5px;
-                        border:1px solid #ccc;
-                        font-size: 15px;
-                    }
-                    .active{
-                        background: #63f8f8;
-                        color:#fff;
-                    }
-                }
-
-            }
-            .footer{
-                 width: 100%;
-                    height: 40px;
-                position: absolute;
-                bottom:0;
-                button{
-                    width: 100%;
-                    height: 40px;
-                    line-height: 40px;
-                    background: linear-gradient(217deg, #f86367, #fb2579);
-                    color:#fff;
-                    text-align: center;
-                    border-radius: 0;
+        // height: 80%;
+        background: #fff;
+        position: absolute;
+        bottom: 0;
+        .header{
+            margin-top:10rpx;
+            display:flex;
+            justify-content:space-between;
+            padding:0 2%;
+            box-sizing:border-box;
+            p{
+                font-size:30rpx;
+                span{
+                    margin-left:5px;
                 }
             }
         }
+        dl{
+            display:flex;
+            padding:0 2%;
+            box-sizing:border-box;
+            dt{
+                margin-right:5px;
+                img{
+                    width:160rpx;
+                    height:160rpx;
+                }
+            }
+            dd{
+                p{
+                    font-size:30rpx;
+                    color:#323a45;
+                    line-height:2;
+                }
+                p:last-child{
+                    color:#999da2;
+                }
+            }
+        }
+        .colors{
+            padding:0 2%;
+            box-sizing:border-box;
+            margin-top:30rpx;
+            p{
+                color:#999da2;
+                font-size:30rpx;
+                line-height:2;
+            }
+            ul{
+                display:flex;
+                flex-wrap:wrap;
+                li{
+                    border:2rpx solid #ccc;
+                    padding:6rpx 16rpx;
+                    box-sizing:border-box;
+                    margin:10rpx 20rpx;
+                    font-size:24rpx;
+                    border-radius:10rpx;
+                }
+                .active1{
+                    background:#33d6c5;
+                    color:#fff;
+                    border:2rpx solid #33d6c5;
+                }
+                .active2{
+                    background:#33d6c5;
+                    color:#fff;
+                    border:2rpx solid #33d6c5;
+                }
+            }
+        }
+        .numbox{
+            padding:0 2%;
+            box-sizing:border-box;
+            display:flex;
+            justify-content:space-between;
+            margin-bottom:20rpx;
+            p{
+                font-size:30rpx;
+                color:#999da2;
+                line-height:2;
+            }
+            .num{
+                display:flex;
+                span{
+                    display:block;
+                    line-height:2;
+                    font-size:28rpx;
+                    text-align:center;
+                    padding:0 20rpx;
+                    box-sizing:border-box;
+                    border:2rpx solid #ccc;
+                }
+                .number{
+                    min-width:80rpx;
+                }
+            }
+        }
+        button{
+            width:100%;
+            height:110rpx;
+            line-height:110rpx;
+            text-align:center;
+            color:#fff;
+            background:linear-gradient(217deg,#f86367,#fb2579);
+        }
     }
+    
+}
 </style>
